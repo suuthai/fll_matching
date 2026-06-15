282353a9b8abe882891f986f1f5b5c270549b1da
suuthai <suuthaisuuthaisuuthaithai@gmail.com>
2026-06-15 21:10:52 +0700

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum :role, { student: 0, instructor: 1, admin: 2 }
+end
