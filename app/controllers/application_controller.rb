f388209736fdb82227140687bf59ac5718fac266
suuthai <suuthaisuuthaisuuthaithai@gmail.com>
2026-07-01 07:57:29 +0700

    サインアウトした後のリダイレクト先をサインイン画面にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def language
     params[:language]
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
